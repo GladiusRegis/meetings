7302b0c88348a043107921eb401f911fa723fbca
Gladius Regis <gladiusregis@gmail.com>
2022-05-06 22:15:00 +0200

    added Calendar and Meeting

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
 from datetime import datetime, timedelta
-
-
-class Meeting:
-    def __init__(self, date: datetime, title: str):
-        self.date = date
-        self.title = title
-
-
-class Calendar:
-    def __init__(self):
-        self.meetings = {}
-
-    def is_available(self, date: datetime):
-        return date not in self.meetings
-
-    def add_meeting(self, meeting: Meeting):
-        if self.is_available(meeting.date):
-            self.meetings[meeting.date] = meeting
-
-    def next_available_slot(self, date: datetime):
-        meeting_date = date
-        while not self.is_available(meeting_date):
-            meeting_date += timedelta(minutes=60)
-        return meeting_date
+from meetings.Meeting import Meeting
+from meetings.Calendar import Calendar
 
 
 def test_check_next_available_slot():
