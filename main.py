8db2b5943f031c44cd3fc9010c1cf4a5b6c50bee
Gladius Regis <gladiusregis@gmail.com>
2022-05-06 23:00:15 +0200

    added menu function

--- main.py
+++ main.py
@@ -2,7 +2,27 @@ from datetime import datetime, timedelta
 from meetings.Meeting import Meeting
 from meetings.Calendar import Calendar
 
+calendar = Calendar()
+
 if __name__ == '__main__':
-    print('Calendar 2022')
+    while True:
+        option = input('What you want to do? [l] - list, [a] - add [q] - quit: ')
+        if option == 'l':
+            for _, meeting in calendar.meetings.items():
+                print(f'{meeting.date}: {meeting.title}')
+
+        elif option == 'a':
+            title = input('The title of the meeting: ')
+            date = input('Date of meeting dd.mm.rrrr h:m: ')
+            meeting_date = datetime.strptime(date, '%d.%m.%Y %H:%M')
+
+            calendar.add_meeting(Meeting(meeting_date, title))
+
+        elif option == 'q':
+            break
+
+        else:
+            print('Use [l] - list, [a] - add [q] - quit ')
+
 
 
